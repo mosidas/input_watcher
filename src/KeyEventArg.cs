c572645d1e2a1e023ccc682d681ee312762d1db0
shota <sspydery0@gmail.com>
2023-01-30 17:01:54 +0900

    init

---
+++ src/KeyEventArg.cs
@@ -0,0 +1,12 @@
+﻿namespace InputWatcher
+{
+    public class KeyEventArg : EventArgs
+    {
+        public int KeyCode { get; }
+
+        public KeyEventArg(int keyCode)
+        {
+            KeyCode = keyCode;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace InputWatcher
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            KeyboardHook keyboardHook = new KeyboardHook(KeyboardHook_KeyDownEvent, KeyboardHook_KeyUpEvent);
+
+            keyboardHook.Hook();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Task.Delay(100);
+                }
+            }).Wait();
+        }
+
+        private static void KeyboardHook_KeyDownEvent(object sender, KeyEventArg e)
+        {
+            Console.WriteLine($"Keyup KeyCode {e.KeyCode}");
+        }
+
+        private static void KeyboardHook_KeyUpEvent(object sender, KeyEventArg e)
+        {
+            Console.WriteLine($"Keydown KeyCode {e.KeyCode}");
+        }
+    }
+}
